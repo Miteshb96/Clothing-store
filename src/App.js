8bebcf1a32f98868e3839a7154d5d645c28411fc
miteshb96 <miteshbhanushali74@gmail.com>
2022-01-06 12:54:07 +0530

    Added React router in scope and created multi-route pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
 
+const HatsComp = () => {
+  return (
+    <>
+      <h1> Hats Page</h1>
+    </>
+  );
+};
+
+const JacketsComp = (props) => {
+  return (
+    <>
+      <h1> Jackets Page</h1>
+    </>
+  );
+};
+
+const SneakersComp = (props) => {
+  return (
+    <>
+      <h1> Sneakers Page</h1>
+    </>
+  );
+};
+
 function App() {
   return (
-    <div>
-      <Homepage />
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Homepage />} />
+        <Route path="/hats" element={<HatsComp />} />
+        <Route path="/jackets" element={<JacketsComp />} />
+        <Route path="/sneakers" element={<SneakersComp />} />
+      </Routes>
+    </Router>
   );
 }
 
